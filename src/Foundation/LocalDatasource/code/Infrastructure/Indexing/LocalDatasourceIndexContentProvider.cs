07d5e456fc4e08786d184491ea2aab726b41c556
Vaggelis Kasapis <v.kasapis@wedia.gr>
2019-05-21 13:17:49 +0300

    Add LocalDatasource Module

---
+++ src/Foundation/LocalDatasource/code/Infrastructure/Indexing/LocalDatasourceIndexContentProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration.Provider;
+using System.Linq.Expressions;
+using Sitecore.ContentSearch.SearchTypes;
+using Sitecore.Data;
+using Wedia.Foundation.Indexing.Infrastructure;
+using Wedia.Foundation.Indexing.Models;
+using Sitecore.Web.UI.WebControls;
+using Sitecore;
+
+namespace Wedia.Foundation.LocalDatasource.Infrastructure.Indexing
+{
+  public class LocalDatasourceQueryPredicateProvider : ProviderBase, IQueryPredicateProvider
+  {
+    public IEnumerable<ID> SupportedTemplates => new[]
+    {
+      TemplateIDs.StandardTemplate
+    };
+
+    public Expression<Func<SearchResultItem, bool>> GetQueryPredicate(IQuery query)
+    {
+      var fieldNames = new[]
+      {
+        Templates.Index.Fields.LocalDatasourceContent_IndexFieldName
+      };
+      return GetFreeTextPredicateService.GetFreeTextPredicate(fieldNames, query);
+    }
+  }
+}
