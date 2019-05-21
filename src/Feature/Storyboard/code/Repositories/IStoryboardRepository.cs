fbc91d6c9d1ec038c4202e3319dc0a101d5643f5
Vaggelis Kasapis <v.kasapis@wedia.gr>
2019-05-21 13:17:10 +0300

    Add Storyboard Module

---
+++ src/Feature/Storyboard/code/Repositories/IStoryboardRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Data.Items;
+
+namespace Wedia.Feature.Storyboard.Repositories
+{
+    public interface IStoryboardRepository
+    {
+        IEnumerable<Item> Get(Item contextItem);
+    }
+}
