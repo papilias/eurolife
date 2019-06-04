1c6e3c99a92c3ea0b7aada0bb0e974e0b2e20ac1
Wedia Sysadmin <sysadmin@wedia.gr>
2019-06-04 10:50:13 +0300

    update style

--- src/Project/Common/code/js/main.js
+++ src/Project/Common/code/js/main.js
@@ -6,24 +6,53 @@
 
     $(document).ready(function () {
 
-        if ($('.js-text-slick').length) {
-            var $pager = $('.text-slick__pager span');
+        if ($('.js-leadership-slider-image').length) {
 
-            $('.js-text-slick').slick({
-                dots: true,
+            $('.js-leadership-slider-image').slick({
+                dots: false,
                 arrows: false,
                 infinite: false,
                 speed: 500,
-                slidesToShow: 1,
-                initialSlide: 1,
-                fade: true,
-                appendDots: $('.reasoning__menu'),
-                customPaging: function (slider, i) {
-                    var item = $pager[i].innerHTML;
-                    return '<a>' + item + '</a>';
-                }
+                slidesToShow: 3,
+                rtl: true,
+                fade: false,
+                asNavFor: '.js-leadership-slider-text',
+                mobileFirst:false,
+                responsive: [
+                    {
+                        breakpoint: 1400,
+                        settings: {
+                            slidesToShow: 2
+                        }
+                    },
+                    {
+                        breakpoint: 1020,
+                        settings: {
+                            slidesToShow: 1
+                        }
+                    }
+                ]
 
             });
+            $('.js-leadership-slider-text').slick({
+                mobileFirst:true,
+                dots: false,
+                arrows: true,
+                infinite: false,
+                speed: 500,
+                slidesToShow: 1,
+                fade: true,
+                asNavFor: '.js-leadership-slider-image',
+                prevArrow: $('.slick-prev'),
+                nextArrow: $('.slick-next')
+
+            });
+
+
+
+
+
+
         }
 
     });
@@ -36,46 +65,50 @@
 
     });
 
-
-    // When the user scrolls the page, execute myFunction
-    window.onscroll = function () {
-        stickyFirst();
-        stickySecond();
-    };
-
-    // Get the header
     var header = document.getElementById("fix-1");
-    var headerHeight = document.getElementById('fix-1').clientHeight;
-    header.style.setProperty('--h1', headerHeight + "px");
-    var stick1 = document.getElementById("after-sticky-1");
-    stick1.style.setProperty('--p1', headerHeight + "px");
-
-
     var menu = document.getElementById("fix-2");
-    var menuHeight = document.getElementById('fix-2').clientHeight;
-    var stick2 = document.getElementById("after-sticky-2");
-    menu.style.setProperty('--h2', menuHeight + "px");
-    menu.style.setProperty('--top', headerHeight + "px");
-    stick2.style.setProperty('--p2', menuHeight + "px");
+    if(header != null && menu != null){
+        // When the user scrolls the page, execute myFunction
+        window.onscroll = function () {
+            stickyFirst();
+            stickySecond();
+        };
 
-    // Get the offset position of the navbar
-    var elementFirstFromTop = header.offsetTop;
-    var elementSecondFromTop = menu.offsetTop;
+        // Get the header
 
-    function stickyFirst() {
-        if ((window.pageYOffset > elementFirstFromTop)) {
-            header.classList.add("sticky");
-        } else {
-            header.classList.remove("sticky");
 
+        var headerHeight = document.getElementById('fix-1').clientHeight;
+        header.style.setProperty('--h1', headerHeight + "px");
+        var stick1 = document.getElementById("after-sticky-1");
+        stick1.style.setProperty('--p1', headerHeight + "px");
+
+
+
+        var menuHeight = document.getElementById('fix-2').clientHeight;
+        var stick2 = document.getElementById("after-sticky-2");
+        menu.style.setProperty('--h2', menuHeight + "px");
+        menu.style.setProperty('--top', headerHeight + "px");
+        stick2.style.setProperty('--p2', menuHeight + "px");
+
+        // Get the offset position of the navbar
+        var elementFirstFromTop = header.offsetTop;
+        var elementSecondFromTop = menu.offsetTop;
+
+        function stickyFirst() {
+            if ((window.pageYOffset > elementFirstFromTop)) {
+                header.classList.add("sticky");
+            } else {
+                header.classList.remove("sticky");
+
+            }
         }
-    }
 
-    function stickySecond() {
-        if (window.pageYOffset > (elementSecondFromTop - headerHeight)) {
-            menu.classList.add("sticky-second");
-        } else {
-            menu.classList.remove("sticky-second");
+        function stickySecond() {
+            if (window.pageYOffset > (elementSecondFromTop - headerHeight)) {
+                menu.classList.add("sticky-second");
+            } else {
+                menu.classList.remove("sticky-second");
+            }
         }
     }
 
@@ -84,10 +117,13 @@
     const leftButton = document.querySelector(".js-program-left");
     const rightButton = document.querySelector(".js-program-right");
     var order = 1;
-
+  //  if(document.getElementById("programs-table") != null)
+       // var programNumber = document.getElementById("programs-table").childElementCount;
     if (leftButton != null && rightButton != null) {
 
         for (const carousel of carousels) {
+
+           // carousel.style.setProperty('--programCount', programNumber);
 
             rightButton.addEventListener('click', _ => {
                 carousel.style.setProperty('--transform', order * (-274 - 32) + "px");
@@ -107,58 +143,57 @@
 
 
     //filtering in products
-    //const filterTrigger = document.querySelector(".filter__trigger");
-    //const filterResults = document.querySelector(".filter__results");
+    const filterTrigger = document.querySelector(".filter__trigger");
+    const filterResults = document.querySelector(".filter__results");
 
-    //if (filterTrigger != null && filterResults != null) {
-
-
-    //    filterTrigger.addEventListener('click', _ => {
-    //        filterResults.classList.toggle("filter__results--active");
-    //    });
+    if (filterTrigger != null && filterResults != null) {
 
 
-    //    const filterClicks = document.querySelectorAll(".filter__item input");
-    //    for (const filterClick of filterClicks) {
-    //        filterClick.addEventListener('click', _ => {
-    //            $('.program').removeClass("program--visible");
-    //            const dataAttr = filterClick.dataset.index;
+        filterTrigger.addEventListener('click', _ => {
+            filterResults.classList.toggle("filter__results--active");
+        });
 
-    //            const checkboxes = document.querySelectorAll(".filter__item input");
-    //            var datas = [];
-    //            //looping checked values and adding to array
-    //            for (const checkbox of checkboxes) {
-    //                if (checkbox.checked == true) {
-    //                    const dataAttr = checkbox.dataset.index;
-    //                    datas.push(dataAttr);
-    //                }
-    //            }
 
-    //            if (datas.length > 0) {
-    //                $('.program').removeClass("program--visible");
-    //                const programs = document.querySelectorAll('.program');
-    //                for (const program of programs) {
-    //                    var programActive = new Boolean(false);
-    //                    var programDatas = program.dataset.characteristics;
-    //                    for (const datum of datas) {
+        const filterClicks = document.querySelectorAll(".filter__item input");
+        for (const filterClick of filterClicks) {
+            filterClick.addEventListener('click', _ => {
+                $('.program').removeClass("program--visible");
 
-    //                        if (programDatas.includes(datum)) {
-    //                            programActive = true;
-    //                        }
+                const checkboxes = document.querySelectorAll(".filter__item input");
+                var datas = [];
+                //looping checked values and adding to array
+                for (const checkbox of checkboxes) {
+                    if (checkbox.checked == true) {
+                        const dataAttr = checkbox.dataset.index;
+                        datas.push(dataAttr);
+                    }
+                }
 
-    //                        if (programActive == true) {
-    //                            program.classList.add('program--visible');
-    //                            console.log(programActive)
-    //                        }
-    //                        else program.classList.remove('program--visible');
-    //                    }
-    //                }
-    //            }
-    //            else $('.program').addClass("program--visible");
+                if (datas.length > 0) {
+                    $('.program').removeClass("program--visible");
+                    const programs = document.querySelectorAll('.program');
+                    for (const program of programs) {
+                        var programActive = new Boolean(false);
+                        var programDatas = program.dataset.characteristics;
+                        for (const datum of datas) {
 
-    //        });
-    //    }
-    //}
+                            if (programDatas.includes(datum)) {
+                                programActive = true;
+                            }
+
+                            if (programActive == true) {
+                                program.classList.add('program--visible');
+                                console.log(programActive)
+                            }
+                            else program.classList.remove('program--visible');
+                        }
+                    }
+                }
+                else $('.program').addClass("program--visible");
+
+            });
+        }
+    }
 
 
 })(jQuery);
