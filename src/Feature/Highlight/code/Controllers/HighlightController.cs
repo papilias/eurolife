776a8fc65f5f1c188cce92fde2519011a2a8cabc
Wedia Sysadmin <sysadmin@wedia.gr>
2019-06-05 13:55:26 +0300

    update highlight

---
+++ src/Feature/Highlight/code/Controllers/HighlightController.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Wedia.Feature.Highlight.Repositories;
+
+namespace Wedia.Feature.Highlight.Controllers
+{
+    public class HighlightController : Controller
+    {  
+        public HighlightController(IHighlightsRepository highlightsRepository)
+        {
+            this.Repository = highlightsRepository;
+        }
+
+        private IHighlightsRepository Repository { get; }
+
+        // GET: Highlights with CTA
+        public ActionResult GetHighlightsCTAItems()
+        {
+            return View("HighlightsSectionCTA", Repository.GetHighlightsCTA(RenderingContext.Current.Rendering.Item));
+        }
+    }
+}
