8405d24c50b8633185b6538748c07081182f64a3
Vaggelis Kasapis <v.kasapis@wedia.gr>
2019-06-19 10:22:02 +0300

    Background Extension

---
+++ src/Foundation/Theming/code/Extensions/ItemExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Sitecore;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Wedia.Foundation.SitecoreExtensions.Extensions;
+
+namespace Wedia.Foundation.Theming.Extensions
+{
+  public static class ItemExtensions
+  {
+    public static string GetBackgroundClass(this Item item, ID field)
+    {
+      if (item == null)
+      {
+        throw new ArgumentNullException(nameof(item));
+      }
+
+      var targetItem = item.TargetItem(field);
+
+      return targetItem?.Fields[Templates.Style.Fields.Class]?.Value ?? "";
+    }
+  }
+}
