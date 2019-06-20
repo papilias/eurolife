b80c69a6f55fac3d86b1c597d100764446f77d22
Vaggelis Kasapis <v.kasapis@wedia.gr>
2019-06-20 13:32:42 +0300

    Add ordering option to search service

--- src/Foundation/Indexing/code/Models/ISearchResult.cs
+++ src/Foundation/Indexing/code/Models/ISearchResult.cs
@@ -3,14 +3,14 @@ using Sitecore.Data.Items;
 
 namespace Wedia.Foundation.Indexing.Models
 {
-    public interface ISearchResult
-    {
-        Item Item { get; }
-        string Title { get; set; }
-        string ContentType { get; set; }
-        string Description { get; set; }
-        Uri Url { get; set; }
-        string ViewName { get; set; }
-        MediaItem Media { get; set; }
-    }
+  public interface ISearchResult
+  {
+    Item Item { get; }
+    string Title { get; set; }
+    string ContentType { get; set; }
+    string Description { get; set; }
+    Uri Url { get; set; }
+    string ViewName { get; set; }
+    MediaItem Media { get; set; }
+  }
 }
--- src/Foundation/Indexing/code/Models/SearchResult.cs
+++ src/Foundation/Indexing/code/Models/SearchResult.cs
@@ -4,33 +4,33 @@ using Wedia.Foundation.SitecoreExtensions.Extensions;
 
 namespace Wedia.Foundation.Indexing.Models
 {
-    public class SearchResult : ISearchResult
+  public class SearchResult : ISearchResult
+  {
+    private Uri _url;
+
+    public SearchResult(Item item)
     {
-        private Uri _url;
-
-        public SearchResult(Item item)
-        {
-            this.Item = item;
-        }
-
-        public Item Item { get; }
-        public MediaItem Media { get; set; }
-        public string Title { get; set; }
-        public string ContentType { get; set; }
-        public string Description { get; set; }
-
-        public Uri Url
-        {
-            get
-            {
-                return this._url ?? new Uri(this.Item.Url(), UriKind.Relative);
-            }
-            set
-            {
-                this._url = value;
-            }
-        }
-
-        public string ViewName { get; set; }
+      this.Item = item;
     }
+
+    public Item Item { get; }
+    public MediaItem Media { get; set; }
+    public string Title { get; set; }
+    public string ContentType { get; set; }
+    public string Description { get; set; }
+
+    public Uri Url
+    {
+      get
+      {
+        return this._url ?? new Uri(this.Item.Url(), UriKind.Relative);
+      }
+      set
+      {
+        this._url = value;
+      }
+    }
+
+    public string ViewName { get; set; }
+  }
 }
