20413f795d130484b961849816e8665970bad208
g.gouvitsas <g.gouvitsas@wedia.gr>
2019-07-11 14:05:10 +0300

    dummy changes in form

--- src/Project/Common/code/webpack.mix.js
+++ src/Project/Common/code/webpack.mix.js
@@ -12,7 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('assets/js/app.js', 'js')
-    //.sass('src/app.scss', 'dist')
+    .sass('src/app.scss', 'dist')
     .setPublicPath('./');
 
 // Full API
