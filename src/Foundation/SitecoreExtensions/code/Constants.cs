f5e3fb40f953752e8737f67fa91548a9fbcc6ffb
Wedia Sysadmin <sysadmin@wedia.gr>
2020-02-17 14:26:36 +0200

    bucket resolver for blog posts - add friendly url
    Former-commit-id: 0a403687133d0d039cd9590beb4fc0ea944e2fff

--- src/Foundation/SitecoreExtensions/code/Constants.cs
+++ src/Foundation/SitecoreExtensions/code/Constants.cs
@@ -1,10 +1,25 @@
-﻿namespace Wedia.Foundation.SitecoreExtensions
+﻿using Sitecore.Data;
+
+namespace Wedia.Foundation.SitecoreExtensions
 {
   public struct Constants
   {
     public struct DynamicPlaceholdersLayoutParameters
     {
       public static string UseStaticPlaceholderNames => "UseStaticPlaceholderNames";
+    }
+
+    public struct BucketResolver
+    {
+      public struct Blog
+      {
+        public const string BlogPath = "/blog/articles";
+      }
+      public struct BlogPost
+      {         
+        public static readonly ID TemplateID = new ID("{67F40FEF-B3C7-4366-B026-183E80DCF4E6}");
+      }
+    
     }
   }
 }
