8634efc9710a92b805451877dd3fa7857296dc58
Wedia Sysadmin <sysadmin@wedia.gr>
2020-03-13 14:10:04 +0200

    menu for blog
    Former-commit-id: 8281f9ea3534c29107dd363c4d87a445acc2ff2d

--- src/Feature/Navigation/code/Constants.cs
+++ src/Feature/Navigation/code/Constants.cs
@@ -1,4 +1,6 @@
-﻿namespace Wedia.Feature.Navigation
+﻿using Sitecore.Data;
+
+namespace Wedia.Feature.Navigation
 {
   public struct Constants
   {
@@ -6,7 +8,8 @@
     {
       public static string Descending => "Descending";
       public static string Limit => "Limit";
-    }
+    }              
 
+    public static readonly ID BlogTemplateID = new ID("{417DA68A-F3A5-4F0E-B96D-8FBFF6FF72E1}");          
   }
 }
