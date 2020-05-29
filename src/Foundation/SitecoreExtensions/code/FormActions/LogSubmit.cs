d3330e0d8363c41e8579c879312dbe4a4ec475f1
Vicky Moschonisiotou <v.moschonisiotou@wedia.gr>
2020-05-29 12:01:42 +0300

    custom form actions
    Former-commit-id: 51feaa1f96331d5ade9ce9f3522df5f8eea8fd86

---
+++ src/Foundation/SitecoreExtensions/code/FormActions/LogSubmit.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceForms.Models;
+using Sitecore.ExperienceForms.Processing;
+using Sitecore.ExperienceForms.Processing.Actions;
+using Sitecore.Data.Items;
+using static System.FormattableString;
+using Sitecore.Links;
+
+
+
+namespace Wedia.Foundation.SitecoreExtensions.FormActions
+{
+  public class LogSubmit : SubmitActionBase<string>
+  {
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LogSubmit"/> class.
+    /// </summary>
+    /// <param name="submitActionData">The submit action data.</param>
+    public LogSubmit(ISubmitActionData submitActionData) : base(submitActionData)
+    {
+    }
+
+    /// <summary>
+    /// Tries to convert the specified <paramref name="value" /> to an instance of the specified target type.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <param name="target">The target object.</param>
+    /// <returns>
+    /// true if <paramref name="value" /> was converted successfully; otherwise, false.
+    /// </returns>
+    protected override bool TryParse(string value, out string target)
+    {
+      target = string.Empty;
+      return true;
+    }
+
+    /// <summary>
+    /// Executes the action with the specified <paramref name="data" />.
+    /// </summary>
+    /// <param name="data">The data.</param>
+    /// <param name="formSubmitContext">The form submit context.</param>
+    /// <returns>
+    ///   <c>true</c> if the action is executed correctly; otherwise <c>false</c>
+    /// </returns>
+    protected override bool Execute(string data, FormSubmitContext formSubmitContext)
+    {
+      Assert.ArgumentNotNull(formSubmitContext, nameof(formSubmitContext));
+
+      if (!formSubmitContext.HasErrors)
+      {
+        Logger.Info(Invariant($"Form {formSubmitContext.FormId} submitted successfully."), this);
+
+  
+        
+        Item formItem = Sitecore.Context.Database.GetItem(Sitecore.Data.ID.Parse(formSubmitContext.FormId));
+        //var pathInfo = LinkManager.GetItemUrl(formItem, UrlOptions.DefaultOptions);
+        //return RedirectToRoute(MvcSettings.SitecoreRouteName, new { pathInfo = pathInfo.TrimStart(new char[] { '/' }), param1 = actionParam });
+
+
+
+      }
+      else
+      {
+        Logger.Warn(Invariant($"Form {formSubmitContext.FormId} submitted with errors: {string.Join(", ", formSubmitContext.Errors.Select(t => t.ErrorMessage))}."), this);
+      }
+
+      return true;
+    }
+  }
+}
---
+++ src/Foundation/SitecoreExtensions/code/FormActions/SendFormValues.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceForms.Models;
+using Sitecore.ExperienceForms.Processing;
+using Sitecore.ExperienceForms.Processing.Actions;
+using Sitecore.Data.Items;
+using static System.FormattableString;
+using Sitecore.Links;
+using Sitecore.ExperienceForms.Processing.Actions.Models;
+using System;
+using Sitecore.Data;
+using Sitecore.Mvc.Extensions;
+
+namespace Wedia.Foundation.SitecoreExtensions.FormActions
+{
+  public class SendFormValues : SubmitActionBase<RedirectActionData>
+  {
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LogSubmit"/> class.
+    /// </summary>
+    /// <param name="submitActionData">The submit action data.</param>
+    public SendFormValues(ISubmitActionData submitActionData) : base(submitActionData)
+    {
+    }
+
+
+
+    /// <summary>
+    /// Executes the action with the specified <paramref name="data" />.
+    /// </summary>
+    /// <param name="data">The data.</param>
+    /// <param name="formSubmitContext">The form submit context.</param>
+    /// <returns>
+    ///   <c>true</c> if the action is executed correctly; otherwise <c>false</c>
+    /// </returns>
+    protected override bool Execute(RedirectActionData data, FormSubmitContext formSubmitContext)
+    {
+      Assert.ArgumentNotNull(formSubmitContext, "formSubmitContext");
+      if (data == null || !(data.ReferenceId != Guid.Empty))
+        return false;
+      var item = Sitecore.Context.Database.GetItem(new ID(data.ReferenceId));
+      if (item == null)
+        return false;
+
+      var email = string.Empty;
+
+      var field = formSubmitContext.Fields.FirstOrDefault(f => f.Name.Equals("Email"));
+      if (field != null)
+      {
+        var property = field.GetType().GetProperty("Value");
+        var postedEmail = property.GetValue(field);
+        email = postedEmail.ToStringOrEmpty();
+      }
+
+      Logger.Info(Invariant($"Email was submitted and value is: {email}"), this);
+      return true;
+    }
+  }
+}
