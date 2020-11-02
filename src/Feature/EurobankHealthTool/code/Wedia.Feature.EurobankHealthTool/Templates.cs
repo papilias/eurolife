dff3dbd03f3b36839dbbe0077da6f35633790c1f
vicky <v.moschonisiotou@wedia.gr>
2020-11-02 14:49:58 +0200

    Qual listing component
    Former-commit-id: b3fa2b76416f0b05434b9c7fe9361cc71825389b

--- src/Feature/EurobankHealthTool/code/Wedia.Feature.EurobankHealthTool/Templates.cs
+++ src/Feature/EurobankHealthTool/code/Wedia.Feature.EurobankHealthTool/Templates.cs
@@ -38,5 +38,34 @@ namespace Wedia.Feature.EurobankHealthTool
       }
     }
 
+
+    public struct EurobankListGroup
+    {
+      public static readonly ID ID = new ID("{0C6604DE-FBCF-49A4-AA94-AD66531AAC3F}");
+
+      public struct Fields
+      {
+        public static readonly ID EurobankList = new ID("{53B43279-8C4F-4FB2-919D-B529FB741E18}");
+        public static readonly ID EurobankListTitle = new ID("{A91447B2-714F-4AF6-988F-CDF3C9F483E9}");
+        public static readonly ID EurobankListSubtitle = new ID("{6A6C3A91-BD74-4438-9789-B9A22780DD54}");
+      }
+    }
+
+    public struct EurobankListMain
+    {
+      public static readonly ID ID = new ID("{D9CD3235-4495-4233-A538-AFE4D8E90552}");
+
+      public struct Fields
+      {
+        public static readonly ID Title = new ID("{D22C0E38-20A0-473C-B815-2C702F14CE4F}");
+        public static readonly ID Summary = new ID("{B177421C-4751-4B76-BD99-E3568B6E4E51}");
+        public static readonly ID Link = new ID("{94DED248-7FDA-43F9-964A-2EDA95BDBC2E}");
+        public static readonly ID Date = new ID("{6FAE2385-76B1-4209-9218-FA40357EB804}");
+        public static readonly ID Category = new ID("{4D3186EA-2A6B-4673-8A57-494637F5B4D5}");
+        public static readonly ID CategoryLink = new ID("{C3B8FF12-1ECB-4E20-BEA4-BBA9EDBCC899}");
+        public static readonly ID Image = new ID("{5AD97C33-C017-47B4-9816-213126CFC29A}");
+      }
+    }
+
   }
 }
